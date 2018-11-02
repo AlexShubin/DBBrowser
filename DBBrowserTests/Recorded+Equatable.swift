3a4055f5b645a1fae1cc09bf84a06665f239d6d0
Alex Shubin <alexshubin22@gmail.com>
2018-11-02 22:27:12 +0300

    pod update

--- DBBrowserTests/Recorded+Equatable.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Copyright © 2018 AlexShubin. All rights reserved.
-//
-
-import RxTest
-import RxSwift
-
-extension RxTest.Recorded: Equatable where Value: Equatable {}
-extension RxSwift.Event: Equatable where Element: Equatable {}
